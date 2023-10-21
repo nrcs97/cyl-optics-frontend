df659d4497793a268144bd1e3e105c64a7fa39b2
nrcs97 <franciscoserracorrea@gmail.com>
2023-10-21 13:32:26 -0300

    improving auth middleware

--- components/AuthRouter/AuthRouter.tsx
+++ components/AuthRouter/AuthRouter.tsx
@@ -1,14 +1,12 @@
 'use client'
 import type { ReactNode } from "react"
-import { useRouter } from "next/navigation"
 import { usePathname } from "next/navigation"
-import { notFound } from 'next/navigation'
+import { notFound } from "next/navigation"
 
-export default function AuthRouter({children, authorization}
-        : { children: ReactNode, authorization: boolean }){
-    const pathname : string[] = usePathname().split('/')
+export default function AuthRouter({children} : { children: ReactNode }){
+    const pathname = usePathname().split('/')
     
-    if(pathname[1] === 'admin' && authorization === false) notFound()
-    
+    if (pathname[1] === 'admin') notFound()
+
     return <>{children}</>
 }
